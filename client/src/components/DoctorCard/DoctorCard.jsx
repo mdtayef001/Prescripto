c24c2ef71c96c07ccfd678d5c4bcae469854fe31
tayef@gain <tayefhossain200@gmail.com>
2025-04-22 04:49:44 +0600

    all done for now

--- client/src/components/DoctorCard/DoctorCard.jsx
+++ client/src/components/DoctorCard/DoctorCard.jsx
@@ -10,8 +10,18 @@ const DoctorCard = ({ doctor }) => {
       <img className="bg-blue-50 w-full" src={doctor.image} alt={doctor.name} />
       <div className="p-4">
         <div className="flex items-center gap-2 text-sm text-green-500">
-          <p className="w-2 h-2 rounded-full bg-green-500"></p>
-          <p>Available</p>
+          <p
+            className={`w-2 h-2 rounded-full ${
+              doctor.available ? "bg-green-500" : "bg-gray-500"
+            } `}
+          ></p>
+          <p
+            className={`${
+              doctor.available ? "text-green-500" : "text-gray-500"
+            } `}
+          >
+            {doctor.available ? "Available" : "Not available"}
+          </p>
         </div>
         <h3 className="text-lg font-medium">{doctor.name}</h3>
         <p className="text-sm text-gray-600">{doctor.speciality}</p>
